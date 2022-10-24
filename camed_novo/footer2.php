c2b0e9180175d597f06ea4a5649cc0ee247ede14
Raphael BrancooI <108133362+branquitoRaph@users.noreply.github.com>
2022-10-24 11:56:51 -0300

    Footer ficar embaixo

--- camed_novo/footer2.php
+++ camed_novo/footer2.php
@@ -1,16 +1,16 @@
 <!-- Criando o rodapé da página-->
-	<footer>
+	<footer class="footer navbar-fixed-bottom">
 		<div id = "copy-area">
 			<div class = "container">
 				<div class = "col-md-12">
-					<p>Desenvolvido por <a href = "#">Ca'Med</a> &copy; 2022</p>
+					<p>Desenvolvido por <a href = "home.php">Ca'Med</a> &copy; 2022</p>
 				</div>
 			</div>
 		</div>
 <!-- Fechando o rodapé-->
-</footer>
-<!-- Scripts do trabalho-->
-<script> src = "js/script.js"</script>
+	</footer>
+	<!-- Scripts do trabalho-->
+	<script> src = "js/script.js"</script>
 <!-- Fechando o corpo da página-->
 </body>
 <!-- Fechando o arquivo html-->
