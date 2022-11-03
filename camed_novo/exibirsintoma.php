2ae76c5e714dc972d7bec953e4629cfd966852f9
Raphael BrancooI <108133362+branquitoRaph@users.noreply.github.com>
2022-11-02 23:15:26 -0300

    Arrumando a tabela
    Tem que ver se está funcionando
    Já está em PDO

--- camed_novo/exibirsintoma.php
+++ camed_novo/exibirsintoma.php
@@ -7,48 +7,53 @@ include_once 'medicamento.php';
 $sintoma= $_POST['sintomas'];
 ?>
 	<!-- Criando a parte principal da página-->
-	<center>
-	<div id="conteudo">
-		<table class="striped">
-		<thead>
-			<tr>
-				<th>Medicamento</th>
-				<th>PMVC</th>
-				<th>Necessita Receita</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php
-			$medicamento = new Medicamento();
-			$tabela= $medicamento->findbySintoma($sintoma);
-			if (count($tabela)>0):
-				foreach($tabela as $linha){
-					echo "Sintoma escolhido: " . $linha['nomeSintoma'];	
-					$receita= $linha['necessarioReceita'];
-					if ($receita==1):
-						$receita="Precisa de receita";
-					else:
-						$receita="Não precisa de receita";
-					endif
-			?>
-			<tr>
-				<td><?php echo $linha['nome'];?></td>
-				<td><?php echo $linha['PMVC'];?></td>
-				<td><?php echo $linha[$receita];?></td>
-			</tr>
-			<?php 
-			} 
-			else: ?>
-				<tr>
-					<td>-</td>
-					<td>-</td>
-					<td>-</td>
-				</tr>
-			<?php endif; ?>
-		</tbody>
-	</table>
+	<div class = "container">
+		<div class = "col-12">
+			<h3 class = "main-title">Sintomas</h3>
+		</div>
+		<div id="conteudo">
+			<center>
+				<table class = "table table-hover">
+					<thead>
+						<tr>
+							<th scope="col">Medicamento</th>
+							<th scope="col">PMVC</th>
+							<th scope="col">Necessita Receita</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+						$medicamento = new Medicamento();
+						$tabela = $medicamento->findbySintoma($sintoma);
+						if (count($tabela)>0):
+							foreach($tabela as $linha){
+								echo "Sintoma escolhido: ". $linha['nomeSintoma'];
+								$receita = $linha['necessarioReceita'];
+								if ($receita==1):
+									$receita="Precisa de receita";
+								else:
+									$receita="Não precisa de receita";
+								endif
+						?>
+						<tr>
+							<td><?php echo $linha['nome'];?></td>
+							<td><?php echo $linha['PMVC'];?></td>
+							<td><?php echo $linha[$receita];?></td>
+						</tr>
+						<?php 
+						} 
+						else: ?>
+						<tr>
+							<td>-</td>
+							<td>-</td>
+							<td>-</td>
+						</tr>
+						<?php endif; ?>
+					</tbody>
+				</table>
+			</center>
+		</div>
 	<!-- Fechando a parte principal da página-->
 	</div>
-	</center>
 	<!-- Criando o rodapé-->
-	<?php include_once 'footer2.php';?>
+	<?php include_once 'footer.php';?>
