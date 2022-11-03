2424e18d32c4c302b495492d7ee59ca6d86bf322
Raphael BrancooI <108133362+branquitoRaph@users.noreply.github.com>
2022-11-02 23:20:17 -0300

    Tá a mesma coisa

--- camed_novo/header.php
+++ camed_novo/header.php
@@ -11,36 +11,6 @@
 	}
 	//Condição para pegar o nome do usuário e guardar em uma variável
 	if($_SESSION['logado'] == true){
-		//Puxa o id com a session da página de login	/*<script type="text/javascript">
-		$("#cep").focusout(function(){
-			//Início do Comando AJAX
-			$.ajax({
-				//O campo URL diz o caminho de onde virá os dados
-				//É importante concatenar o valor digitado no CEP
-				url: 'https://viacep.com.br/ws/'+$(this).val()+'/json/unicode/',
-				//Aqui você deve preencher o tipo de dados que será lido,
-				//no caso, estamos lendo JSON.
-				dataType: 'json',
-				//SUCESS é referente a função que será executada caso
-				//ele consiga ler a fonte de dados com sucesso.
-				//O parâmetro dentro da função se refere ao nome da variável
-				//que você vai dar para ler esse objeto.
-				success: function(resposta){
-					//Agora basta definir os valores que você deseja preencher
-					//automaticamente nos campos acima.
-					$("#logradouro").val(resposta.logradouro);
-					$("#complemento").val(resposta.complemento);
-					$("#bairro").val(resposta.bairro);
-					$("#cidade").val(resposta.localidade);
-					$("#uf").val(resposta.uf);
-					//Vamos incluir para que o Número seja focado automaticamente
-					//melhorando a experiência do usuário
-					$("#numero").focus();
-				}
-			});
-		});
-	</script>
-	*/
 		$id = (int) $_SESSION['id'];
 		//Comando sql para selecionar o nome do usuário a partir do id do usuário logado
 		$sql = "SELECT  nomeUsuario from usuario where $id = idUsuario;";
