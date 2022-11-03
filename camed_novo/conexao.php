aa323d5054f673cebad5dc2fcd0cb99e80175457
Raphael BrancooI <108133362+branquitoRaph@users.noreply.github.com>
2022-11-02 23:06:19 -0300

    Nem mexi em nada

--- camed_novo/conexao.php
+++ camed_novo/conexao.php
@@ -2,7 +2,7 @@
 //Dados do usuário, pasta host, senha (se tiver) e o nome do banco de dados
 define("HOST", "localhost");
 define("USUARIO", "root");
-define("SENHA", "usbw");
+define("SENHA", "");
 define("DB", "camed");
 
 //Variável que inicia a conexão com a função msqli_connect, se não for possível irá mostrar a mensagem
