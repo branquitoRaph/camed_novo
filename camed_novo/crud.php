fd43af8c76e90d9d3e520ac34272a4e7e6c2ae07
Raphael BrancooI <108133362+branquitoRaph@users.noreply.github.com>
2022-11-02 23:11:29 -0300

    Comentei a função de update

--- camed_novo/crud.php
+++ camed_novo/crud.php
@@ -5,7 +5,7 @@
 		protected $table;
 
 		abstract public function insert();
-		abstract public function update($id);
+		//abstract public function update($id);
 		
 		public function  find($id){
 			$sql = "SELECT * FROM $this->table WHERE id = :id";
@@ -13,16 +13,14 @@
 			$stmt->bindParam(':id', $id, PDO::PARAM_INT);
 			$stmt->execute();
 			return $stmt->fetch(PDO::FETCH_BOTH);
-			
 		}
 		
 		public function  findAll(){
 			$sql = "SELECT * FROM $this->table ";
-			$stmt = Database::prepare($sql);			
+			$stmt = Database::prepare($sql);
 			$stmt->execute();
 			//retorna um array com os registros da tabela indexado pelo nome da coluna da tabela e por um número
 			return $stmt->fetchAll(PDO::FETCH_BOTH );
-		    
 		}
 		
 		public function delete($id){
@@ -30,7 +28,6 @@
 			$stmt = Database::prepare($sql);	
 			$stmt->bindParam(':id', $id, PDO::PARAM_INT);
 			return $stmt->execute();
-			
 		}
 		
 	}
