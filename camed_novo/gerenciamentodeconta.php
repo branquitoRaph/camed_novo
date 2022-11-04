84fd5e2e9d66a0219be12824bf60f2d02961a76d
Raphael BrancooI <108133362+branquitoRaph@users.noreply.github.com>
2022-11-03 22:55:26 -0300

    Arrumei a tabela e concertei os erros
    Falta passar para PDO

--- camed_novo/gerenciamentodeconta.php
+++ camed_novo/gerenciamentodeconta.php
@@ -2,7 +2,7 @@
 //Chamando o arquivo de conexão
 require_once 'conexao.php';
 if(session_status()=== PHP_SESSION_NONE){
-			session_start();
+	session_start();
 	}
 	//Condição para pegar o nome do usuário e guardar em uma variável
 	if($_SESSION['logado'] == true){
@@ -12,59 +12,69 @@ if(session_status()=== PHP_SESSION_NONE){
 ?>
 <!-- Chamando o cabeçalho da página-->
 <?php include_once 'header.php';?>
+<br>
+<br>
 <!-- Div para separar o arquivo, afim de organização -->
-<div class="row">
-	<!-- Quebra de linha -->
-	<br>
-	<h1><p>Gerenciamento de Conta</p></h1>
-	<!-- Centralizando e criando a tabela -->
-	<center>
-		<tbody>
-			<?php
-			//Comando para pegar os dados da tabela medicamento
-			$sql="SELECT idUsuario, nomeUsuario, sobrenomeUsuario, cepUsuario, emailUsuario, senhaUsuario from usuario WHERE idUsuario = $id";
-			//Pegando o resultado do comando
-			$resultado= mysqli_query($conexao,$sql);
-			//Condição se o número de linhas é maior que 0
-			if (mysqli_num_rows($resultado)>0):
-				//Loop para guardar os dados em uma variável
-				while($dados =mysqli_fetch_array($resultado)):
-?>
-			<!-- Escreve os dados -->
-			<div class="display">
-				<input disabled value="<?php  echo $dados['nomeUsuario'];?>">
-				<input disabled value="<?php  echo $dados['sobrenomeUsuario'];?>">
-				<input disabled value="<?php  echo $dados['cepUsuario'];?>">
-				<input disabled value="<?php  echo $dados['emailUsuario'];?>">
-				<input disabled value="<?php  echo $dados['senhaUsuario'];?>">
-			</div>
-<?php 
-			//Fecha o loop
-			 endwhile;
-			//Se não, tabela vazia
-			else: ?>
-				<tr class="tr">
-					<td>-</td>
-					<td>-</td>
-					<td>-</td>
-					<td>-</td>
-				</tr>
-<?php
-			//Fecha condição
-			endif;
-?>
-		<!-- Fecha o corpo da tabela -->
-		</tbody>
-	<!-- Fecha a tabela e a centralização-->
-	</center>
-	<br>
-<!-- Fecha a div -->
-</div>
-	<br>
-	<br>
-	<!-- Parágrafo para fazer cadastro, caso não tenha login -->
-	<p>Deseja alterar alguma informação de sua conta? <a href="alterar.php">Clique aqui</a></p>
-	<!-- Parágrafo para fazer cadastro, caso não tenha login -->
-	<p>Deseja apagar sua conta? <a href="excluir.php">Clique aqui</a></p>
+	<div class = "container">
+		<div class = "col-12">
+			<h3 class = "main-title">Gerenciamento de Conta</h3>
+		</div>
+		<div id = "conteudo">
+			<!-- Centralizando e criando a tabela -->
+			<center>
+				<table class = "table table-hover">
+					<thead>
+							<tr>
+								<th scope="col">Nome</th>
+								<th scope="col">Sobrenome</th>
+								<th scope="col">Data de Nascimento</th>
+								<th scope="col">E-mail</th>
+								<th scope="col">Senha</th>
+							</tr>
+					</thead>
+					<tbody>
+						<?php
+						//Comando para pegar os dados da tabela medicamento
+						$sql="SELECT idUsuario, nomeUsuario, senha, dataNascimento, sobrenomeUsuario, emailUsuario from usuario WHERE idUsuario = $id";
+						//Pegando o resultado do comando
+						$resultado= mysqli_query($conexao,$sql);
+						//Condição se o número de linhas é maior que 0
+						if (mysqli_num_rows($resultado)>0):
+							//Loop para guardar os dados em uma variável
+							while($dados =mysqli_fetch_array($resultado)):
+						?>
+						<tr>
+							<td><?php echo $dados['nomeUsuario'];?></td>
+							<td><?php echo $dados['sobrenomeUsuario'];?></td>
+							<td><?php echo $dados['dataNascimento'];?></td>
+							<td><?php echo $dados['emailUsuario'];?></td>
+							<td><?php echo $dados['senha'];?></td>
+						</tr>
+						<?php 
+						//Fecha o loop
+						endwhile;
+						//Se não, tabela vazia
+						else: ?>
+						<tr class="tr">
+							<td>-</td>
+							<td>-</td>
+							<td>-</td>
+							<td>-</td>
+							<td>-</td>
+						</tr>
+						<?php
+						//Fecha condição
+						endif;
+						?>
+						<!-- Fecha o corpo da tabela -->
+					</tbody>
+				<!-- Fecha a tabela e a centralização-->
+				</table>
+				<p>Deseja alterar alguma informação de sua conta? <a href="alterar.php">Clique aqui</a></p>
+				<p>Deseja apagar sua conta? <a href="excluir.php">Clique aqui</a></p>
+			</center>
+		</div>
+	<!-- Fecha a div -->
+	</div>
 <!-- Chamando o rodapé-->
 <?php include_once 'footer.php';?>
