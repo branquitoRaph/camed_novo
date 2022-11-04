f6b44f9c82f0593ec43b0900f8db2ce37c37aa34
Raphael BrancooI <108133362+branquitoRaph@users.noreply.github.com>
2022-11-03 22:59:02 -0300

    Só passar para PDO e mais campos

--- camed_novo/alterar.php
+++ camed_novo/alterar.php
@@ -16,19 +16,18 @@ if(isset($_POST['Enviar'])):
 		$id = (int) $_SESSION['id'];
 	}
 	//$senha=md5(mysqli_escape_string($conexao,$_POST['senha']));	
-	$sql="UPDATE usuario SET senhaUsuario ='$senha' WHERE idUsuario=$id";
+	$sql="UPDATE usuario SET senha ='$senha' WHERE idUsuario=$id";
 	if(mysqli_query($conexao,$sql)):
-		$_SESSION['mensagem'] = "Excluido com sucesso!";
-		header('Location: ../rootTrab/login.php');
+		$_SESSION['mensagem'] = "Senha alterada com sucesso!";
+		header('Location: login.php');
 	else:
-		$_SESSION['mensagem'] = "Erro ao excluir!";
-		header('Location: ../rootTrab/login.php');
+		$_SESSION['mensagem'] = "Erro ao alterar senha!";
+		header('Location: alterar.php');
 	endif;
 endif;		
-?>
-<!-- Chamando o cabeçalho da página -->
-<?php include_once 'header.php';?>
-<?php
+
+//<!-- Chamando o cabeçalho da página -->
+include_once 'header.php';
 	//Condição de checamento de dados
 	if(!empty($erros)):
 		//Loop de erro
@@ -41,18 +40,25 @@ endif;
 	endif;
 ?>
 	<!-- Abrindo um formulário para alterar dados-->
-	<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-		<!-- Texto que aparece-->
-		<label for = "pwd">Nova Senha:</label>
-		<!-- Campo de digitação-->
-		<input type = "password" id = "pwd" name = "senha" minlength = "8" placeholder = "Nova Senha" <br><br><br>
-		<!-- Campo de enviar-->
-		<input type = "submit" name = "Enviar" value = "Confirmar">
-	<!-- Fechando o formulário-->
-	</form>
-	<!-- Quebra de linha -->
-	<br>
-	<!-- Parágrafo para fazer cadastro, caso não tenha login -->
-	<p>Deseja voltar ao gerenciamento de sua conta? <a href="gerenciamentodeconta.php">Clique aqui</a></p>
+	<div id = "alterar">
+		<br>
+		<br>
+		<div class = "container">
+			<div class = "row açign-items-center">
+				<div class = "col-md-10 mx-auto col-lg-5">
+					<h3 class = "main-title">Alterar Dados</h3>
+					<br>
+					<form class = "p-4 p-md-5 border rounded-3 bg-light" method = "POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+						<div class="form-group">
+							<label for="pwd">Nova Senha</label>
+							<input type="password" id = "pwd" class="form-control" id="email" minlength = "8" name = "senha" placeholder="Nova Senha">
+						</div>
+						<input type="submit" class="btn btn-primary btn-lg btn-block" name = "Enviar" value = "Confirmar">
+					</form>
+					<p>Deseja voltar ao gerenciamento de sua conta? <a href="gerenciamentodeconta.php">Clique aqui</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
 <!-- Chamando o rodapé-->
 <?php include_once 'footer.php';?>
