bc05c1f6c8c8a85c581b89976167f6721b149afc
Raphael Branco <108133362+branquitoRaph@users.noreply.github.com>
2022-12-07 18:00:24 -0300

    a bosta do user não tinha

--- camed_novo/mobile/conexao.php
+++ camed_novo/mobile/conexao.php
@@ -2,13 +2,14 @@
 
 // Abre uma conexao com o BD.
 
-$host        = "host = localhost";
-$port        = "port = 3306;";
-$dbname      = "DB = camed;";
-$dbpassword	 = "";
+$host        = "host = 127.0.0.1;";
+$port        = "port = 3307;";
+$dbname      = "dbname = camed;";
+$dbuser 	 = "root";
+$dbpassword	 = "usbw";
 
 // para conectar ao mysql, substitua pgsql por mysql
-$db_con= new PDO('mysql:' . $host . $port . $dbname, $dbpassword);
+$db_con= new PDO('mysql:' . $host . $port . $dbname, $dbuser, $dbpassword);
 
 //alguns atributos de performance.
 $db_con->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
